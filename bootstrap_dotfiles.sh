259410ca1606cffa4020ed9921bab86529654b15
Jakob Kofoed Janot <jakob@janot.dk>
2015-02-01 12:15:40 +0100

    Removed bash_aliases from bootstrap script

--- bootstrap_dotfiles.sh
+++ bootstrap_dotfiles.sh
@@ -5,8 +5,6 @@ echo "Backup"
 mkdir ~/dotfiles_backup
 
 mv ~/.profile ~/dotfiles_backup
-mv ~/.bash_aliases ~/dotfiles_backup
-mv ~/.bash ~/dotfiles_backup
 mv ~/.atom ~/dotfiles_backup
 mv ~/.gitignore ~/dotfiles_backup
 mv ~/.gitconfig ~/dotfiles_backup
@@ -15,8 +13,6 @@ mv ~/.bundle ~/dotfiles_backup
 echo "Creating symlinks"
 
 ln -s dotfiles/profile ~/.profile
-ln -s dotfiles/bash_aliases ~/.bash_aliases
-ln -s dotfiles/bash ~/.bash
 ln -s dotfiles/atom ~/.atom
 ln -s dotfiles/gitignore ~/.gitignore
 ln -s dotfiles/gitconfig ~/.gitconfig
